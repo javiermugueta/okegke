940ce3288a776ff76200c911d1cbb1c527d94fa7
javi <javiermugueta@gmail.com>
2019-07-04 20:14:22 +0200

    .

--- hello.js
+++ hello.js
@@ -2,7 +2,8 @@ const FROMWHERE = process.env.FROMWHERE
 var http = require('http');
 var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World from " + FROMWHERE + "!");
+    counter = counter +1;
+    response.end("{\"message\": \"Hello World from " + FROMWHERE + "!<br>It seems folks have been here up to " + counter +  " times \"}");
 });
 var port = 80;
 server.listen(port);
